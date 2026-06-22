ca703403236c1475f328e752d0240ad01a1f8150
saumya452 <saumyajanbandhu10@gmail.com>
2026-06-22 13:47:25 +0530

    Add modules for cyber security toolkit options

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import password_generator
+import password_strength_checker
+import hash_generator
+import port_scanner
+import dns_lookup
+import whois_lookup
+import file_integrity_checker
+
 print("=== Cyber Security Toolkit ===")
 
 print("1. Password Generator")
@@ -7,8 +15,22 @@ print("4. Port Scanner")
 print("5. DNS Lookup")
 print("6. WHOIS Lookup")
 print("7. File Integrity Checker")
-print("8. Exit")
 
 choice = input("\nSelect an option: ")
 
-print(f"You selected option {choice}")
+if choice == "1":
+    password_generator
+elif choice == "2":
+    password_strength_checker
+elif choice == "3":
+    hash_generator
+elif choice == "4":
+    port_scanner
+elif choice == "5":
+    dns_lookup
+elif choice == "6":
+    whois_lookup
+elif choice == "7":
+    file_integrity_checker
+else:
+    print("Invalid option")
