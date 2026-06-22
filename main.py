f8349880b69d08dce859329f7b9d25f1d3fbcd17
saumya452 <saumyajanbandhu10@gmail.com>
2026-06-22 13:45:37 +0530

    Add Cyber Security Toolkit main menu

---
+++ main.py
@@ -0,0 +1,14 @@
+print("=== Cyber Security Toolkit ===")
+
+print("1. Password Generator")
+print("2. Password Strength Checker")
+print("3. Hash Generator")
+print("4. Port Scanner")
+print("5. DNS Lookup")
+print("6. WHOIS Lookup")
+print("7. File Integrity Checker")
+print("8. Exit")
+
+choice = input("\nSelect an option: ")
+
+print(f"You selected option {choice}")
